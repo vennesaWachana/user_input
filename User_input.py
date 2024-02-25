9ea7063d0d3ba4491faf0ad7ee28e1207aa1ed54
vanesa wachana <138315720+vennesaWachana@users.noreply.github.com>
2024-02-25 12:18:12 +0300

    Add files via upload

---
+++ User_input.py
@@ -0,0 +1,16 @@
+# Ask for user's name
+name = input("vanesa ")
+
+# Ask for user's age
+age = input("21 ")
+
+# Ask for user's location
+location = input("Nairobi ")
+
+# Create personalized message
+message = f"have an nice day stay postive"
+# Print out personalized message
+print(name)
+print(age)
+print(location)
+print(message)
